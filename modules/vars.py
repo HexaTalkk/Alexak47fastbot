76c42d6228fb6d4ba8bb95e74f9f6deb95e0a367
Chutiyahokaa <156505036+Chutiyahokaa@users.noreply.github.com>
2024-04-09 17:13:46 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "20319884"
 api_hash = "637e3ba6357aa3ba2f3bf5742e0fd066"
-bot_token = "6458458851:AAHFMRhWnxkY4FIkd4VZng2Oxn7rIT-Kl8Y"
+bot_token = "6823140439:AAEn8drCNncmt0UV8O8ojld6r3gbN7hWk3A"
